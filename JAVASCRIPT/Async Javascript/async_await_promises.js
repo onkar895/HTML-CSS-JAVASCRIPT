148d393ed3021517d59985aaaf2a0e62d78ca164
Onkar Karale <onkarkarale4@gmail.com>
2023-04-11 02:59:59 +0530

    Async/Await in Javascript

--- JAVASCRIPT/Async Javascript/async_await_promises.js
+++ JAVASCRIPT/Async Javascript/async_await_promises.js
@@ -17,6 +17,7 @@ async function getData() {
 
 getData();
 
+// async functions always returns the promise, other values are wrapped inside the promise automatically.
 // In the above example ,   the getData function is declared as an asynchronous function using the async keyword.
 // Inside the asynchronous function, we use the await keyword to wait for the fetch function to complete and retrieve some data from an API.
 // Once the data is retrieved, we use await again to wait and parse the retrieved data as JSON.
@@ -45,3 +46,85 @@ async function msg1() {
 
 msg();
 msg1();
+
+
+// Example 3:
+
+
+async function Weather() {
+    let puneWeather = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("27 degrees");
+        }, 2000)
+    })
+
+    let mumbaiWeather = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("21 degrees");
+        }, 5000)
+    })
+
+    console.log("Fetching pune weather plz wait....")
+    let puneW = await puneWeather;
+    console.log("Fetched pune Weather :" + puneW);
+
+    console.log("Fetching mumbai weather plz wait....")
+    let mumbaiW = await mumbaiWeather;
+    console.log("Fetched mumbai Weather :" + mumbaiW);
+
+    return [puneW, mumbaiW];
+
+}
+
+const Wait = async() => {
+    console.log("HI, I'm Waiting")
+}
+
+const main1 = async() => {
+    console.log("Welcome to weather control room")
+    let a = await Weather();
+    let b = await Wait()
+        // let x = Weather();
+        // x.then((val) => {
+        //     console.log(val)
+        // })
+}
+
+main1();
+
+
+// Async Function Error Handling:
+
+// 1. JavaScript async functions uses try...catch statements for error handling.
+// 2. This method allows shared error handling for synchronous and asynchronous code.
+
+// The try...catch syntax has two main blocks that is try and catch.
+// try and catch works like this:
+//    1. Firsty the code inside the try is executed.
+//    2. If there is no errors catch is ignored else catch will be executed.
+
+
+setTimeout(() => {
+    console.log("Hacking wifi....plz wait....")
+}, 1000);
+
+// handling error : so if u want to show existing error but u also want to be execute furhter code then u should use go with try...catch concept
+// Rememeber that inside try we can only handle synchronous code.
+try {
+    console.log(rahul) // Error
+} catch (error) {
+    // console.log(error);
+    console.log("Invalid data");
+}
+
+setTimeout(() => {
+    console.log("Fetching username and password....plz wait....")
+}, 3000);
+
+setTimeout(() => {
+    console.log("Hacking rahul's insta id....plz wait....")
+}, 5000);
+
+setTimeout(() => {
+    console.log("Username and password of rahul (dwar123456) fetched.");
+}, 7000);
